0a6e1c0c22b4b76ff120c62d807460504d72143f
Jack Anteby <Janteby1@gmail.com>
2016-03-28 16:58:34 -0400

    working google  geocoding and maps functionality

--- dates/static/dates/main.js
+++ dates/static/dates/main.js
@@ -1,3 +1,24 @@
+function init_map(map_date, latitude, longitude) {
+    // this gets the lat and long from the geocoder below
+    var var_location = new google.maps.LatLng(latitude, longitude);
+ 
+    var var_mapoptions = {
+        center: var_location,
+        zoom: 16
+    };
+ 
+    var var_marker = new google.maps.Marker({
+    position: var_location,
+    map: var_map });
+ 
+    var var_map = new google.maps.Map(map_date.get(0),
+        var_mapoptions);
+    // the zero will get the element the jquery object is wrapping
+ 
+    var_marker.setMap(var_map); 
+};
+
+
 $(document).ready(function(){
   console.log("Hi there!")
 
@@ -22,7 +43,7 @@ $(document).ready(function(){
     		var template = $('#details').html();
 			// we get an object with a property comments, so here we call data.comments or just pass the data
 			var renderM = Mustache.render(template,data);
-			console.log(renderM); //for testing 
+			// console.log(renderM); //for testing 
 
 
             var date_slug = (data.dates[0].slug) // gets the post fk from the comment data we send back
@@ -34,5 +55,31 @@ $(document).ready(function(){
 			}
 		})
 	})
+
+    $('.map_button').on('submit', function(event){
+
+        event.preventDefault();
+        var date_id = ($(this).find("[name='date_id']").attr("value")); // find tells it where in the this object to look for the value
+        var map_date = $("#" + date_id) // targets the right div in the DOM that has the same post id as ou post FK
+        
+
+        var geocoder = new google.maps.Geocoder();
+        var address = ($(this).find("[name='address']").attr("value")); // get the address from the DOM of the buttom you clicked
+
+        geocoder.geocode({'address': address}, function(results, status) {
+            // this takes the address we got from the DOM and uses google api to get the geocode, then send the geocode to the google maps api
+            if (status == google.maps.GeocoderStatus.OK) {
+                var latitude = results[0].geometry.location.lat();
+                var longitude = results[0].geometry.location.lng();
+                // console.log(latitude);
+                // console.log(longitude);
+                } 
+
+            map_date.css({"height":"250px"});
+         
+            init_map(map_date, latitude, longitude);
+
+        }); 
+    })
 });
 
